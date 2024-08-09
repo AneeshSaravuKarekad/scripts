d1ba247209928237d3526e6f2c576437c8988af6
Aneesh SK <aneeshsk1996@gmail.com>
2024-08-09 12:42:45 -0400

    added hyprland install script

---
+++ bash/hyprland/pre_setup.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+set -e
+
+source ~/scripts/bash/colors.sh
+
+install_dependecies() {
+    set -e
+    echo -e "${GREEN}[INFO]${NC} Installing hyprland dependencies..."
+    
+    sudo pacman -S --needed gdb ninja gcc cmake meson cpio tomlplusplus \
+    xcb-util-wm xcb-proto xcb-util xcb-util-keysyms xcb-util-errors \
+    libxcb libxfixes libx11 libxcomposite libxrender libxkbcommon \
+    pixman wayland-protocols cairo pango seatd \
+    xorg-xwayland xorg-xinput \
+    libinput libliftoff libdisplay-info \
+    hyprlang hyprcursor hyprwayland-scanner aquamarine hyprutils 
+    
+    # paru -S --needed hyprutils-git
+}
+
+install_hyprland() {
+    set -e
+    echo -e "${GREEN}[INFO]${NC} Cloning hyprland source..."
+    git clone --recursive https://github.com/hyprwm/Hyprland ~/software/hyprland
+    
+    echo -e "${GREEN}[INFO]${NC} Installing Hyprland..."
+    cd hyprland && make all && sudo make install
+}
+
+install_dependecies
+install_hyprland
