484420356b23ea1be4c47447d7a9eb08cd7e8d49
Aneesh SK <aneeshsk1996@gmail.com>
2024-08-09 22:56:32 -0400

    refactor: move lang util setup to top

--- bash/init.sh
+++ bash/init.sh
@@ -3,7 +3,7 @@
 set -e
 
 source ~/scripts/bash/xdg/setup.sh
+source ~/scripts/bash/setup_lang_utils.sh
 source ~/scripts/bash/setup_package_manager.sh
 sorce ~/scripts/bash/utils/setup.sh
-source ~/scripts/bash/setup_lang_utils.sh
 source ~/scripts/bash/hyprland/setup.sh
